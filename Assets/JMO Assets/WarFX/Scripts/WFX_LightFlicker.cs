4c4afe37ae0f723a72f394c71ab3147e3773a287
falcon1201 <furkanduyan@hotmail.com>
2024-03-16 23:08:49 +0300

    Character Controller

---
+++ Assets/JMO Assets/WarFX/Scripts/WFX_LightFlicker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ *	Rapidly sets a light on/off.
+ *	
+ *	(c) 2015, Jean Moreno
+**/
+
+[RequireComponent(typeof(Light))]
+public class WFX_LightFlicker : MonoBehaviour
+{
+    public float time = 0.05f;
+
+    void Start()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        GetComponent<Light>().enabled = true;
+        yield return new WaitForSeconds(time);
+        GetComponent<Light>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Guns/Pistol.cs
@@ -0,0 +1,12 @@
+using Guizzan.Input.GIM.Guns;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : BaseGun
+{
+    public override void OnGunStateChanged(GunInputs state)
+    {
+    
+    }
+}
