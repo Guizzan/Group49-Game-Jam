c92dbe5024d8f926509f0b72d4db1f781abeeb4c
Глеб Чубаров <gchubarov@Glebs-MacBook-Air.local>
2024-03-17 20:56:22 +0300

    IDamageable

---
+++ Assets/Scripts/Other/Damage/BulletDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    public float radius;
+    private bool damageDealt = false;
+
+    private void Update()
+    {
+        if (damageDealt)
+            return;
+
+        Vector3 damagePos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(damagePos, radius);
+
+        foreach (Collider hit in colliders)
+        {
+            if (hit.TryGetComponent<IDamageable>(out IDamageable damageable))
+            {
+                if (damageable != null)
+                    damageable.GetDamage();
+                damageDealt = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Other/Damage/DamageableProp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableProp : MonoBehaviour, IDamageable
+{
+    public float radius = 0.5F;
+    public float power = 2.0F;
+
+    public void GetDamage()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            if (hit.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                if (rb != null)
+                    rb.isKinematic = false;
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+            }
+
+
+        }
+    }
+}
